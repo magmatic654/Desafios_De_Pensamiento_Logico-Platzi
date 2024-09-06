0fb253b0e327208dc147cb62f3e9b01b6580b9ec
Harold Navarro <kaendor50@gmail.com>
2024-09-06 13:24:59 -0600

    Desafio 03 - Carrera de velocidad

---
+++ Carrera_De_Automoviles.py
@@ -0,0 +1,11 @@
+# Descripción del reto
+# Tenemos información de que el conductor José ha llegado justo detrás de Pedro, además de que Miguel  llegó en medio de Francisco y José.
+
+# ¿Cuál es el orden de llegada de los conductores?
+
+# El orden de llegada es:
+
+# Jose < Pedro
+# Francisco < Miguel < Jose 
+
+# 1. Pedro, 2. Jose, 3. Miguel, 4. Francisco
