cba670dc394c501c726ecc9eca31a9a0081a1b09
Harold Navarro <kaendor50@gmail.com>
2024-09-06 13:11:44 -0600

    Desafio - Caja de frutas

---
+++ Caja_de_frutas.py
@@ -0,0 +1,9 @@
+# Descripción del reto
+# Tengo 3 cajas, cada una contiene un tipo de fruta diferente (peras, manzanas y naranjas). El problema es que todas las cajas tienen las etiquetas de las frutas incorrectas. ¿Cuántas cajas necesito abrir para saber colocar todas las etiquetas correctamente?
+
+# 🍐 Peras
+# 🍎 Manzanas
+# 🍊 Naranjas
+
+# SOLUCION
+# Se necesita una sola caja abierta, pues al abrir una caja descartamos inmediatamente la posibilidad de la caja abierta, intercambiamos la etiqueta de la caja abierta por la fruta que encontramos y finalmente intercambiamos las etiquetas de las cajas no abiertas consiguiendo en orden correcto para todas.
